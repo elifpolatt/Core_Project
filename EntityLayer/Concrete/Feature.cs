b33639e6c60365da9f3df5b0576d45b44a1fcc83
Elif Polat <elif7polat@gmail.com>
2023-12-07 16:52:29 +0300

    migration

--- EntityLayer/Concrete/Feature.cs
+++ EntityLayer/Concrete/Feature.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Principal;
 using System.Text;
@@ -9,6 +10,7 @@ namespace EntityLayer.Concrete
 {
     public class Feature
     {
+        [Key]
         public int Id { get; set; }
 
         public string Header { get; set; }
