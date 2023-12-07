f32b23daf795a68e58da54ff2889e7372601c4d4
Elif Polat <elif7polat@gmail.com>
2023-12-07 14:22:41 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature
+    {
+        public int Id { get; set; }
+
+        public string Header { get; set; }
+        public string Name{ get; set; }
+        public string Title { get; set; }
+    }
+}
