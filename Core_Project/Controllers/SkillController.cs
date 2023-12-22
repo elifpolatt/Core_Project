0db33419a3ade718edcd082026bfbcb757ecbf7d
Elif Polat <elif7polat@gmail.com>
2023-12-22 18:56:04 +0300

    creating partial

---
+++ Core_Project/Controllers/SkillController.cs
@@ -0,0 +1,30 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class SkillController : Controller
+    {
+        private SkillManager skillManager = new SkillManager(new EfSkillDal());
+        public IActionResult Index()
+        {
+            var values = skillManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill)
+        {
+            skillManager.TAdd(skill);
+            return View("Index");
+        }
+    }
+}
